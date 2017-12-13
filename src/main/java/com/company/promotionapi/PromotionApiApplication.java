31f625648691b1f58390d4998c502dca226b97be
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2017-12-13 20:12:17 -0200

    First commit!

---
+++ src/main/java/com/company/promotionapi/PromotionApiApplication.java
@@ -0,0 +1,12 @@
+package com.company.promotionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionApiApplication.class, args);
+	}
+}
