4bc410fb767c6afacc5abec33afe6ab7d1908f52
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2017-12-15 23:08:32 -0200

    Fix and test notifyUpdatedCampaign

--- src/main/java/com/company/promotionapi/listener/CampaignMessageListener.java
+++ src/main/java/com/company/promotionapi/listener/CampaignMessageListener.java
@@ -1,7 +1,10 @@
 package com.company.promotionapi.listener;
 
 import com.company.promotionapi.model.Campaign;
-import com.sun.javafx.binding.StringFormatter;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,8 +13,13 @@ import java.util.logging.Logger;
 @Component
 public class CampaignMessageListener {
 
+    public final static String UPDATE_CAMPAIGN_MESSAGE_QUEUE = "UPDATE_CAMPAIGN_QUEUE";
+
     private static final Logger log = Logger.getLogger(CampaignMessageListener.class.getName());
 
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(exclusive = "true", durable = "false"),
+            exchange = @Exchange(value = UPDATE_CAMPAIGN_MESSAGE_QUEUE, type = "fanout", durable = "true"))
+    )
     public void receiveMessage(List<Campaign> campaigns) {
         log.info("Received");
         log.info("Message processed..."+ campaigns);
